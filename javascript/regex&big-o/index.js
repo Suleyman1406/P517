9d694226a471c9527ec286601f2a6affe16e2328
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-06-18 20:24:15 +0400

    regex  lesson added

---
+++ javascript/regex&big-o/index.js
@@ -0,0 +1,44 @@
+// let pattern = /^[a-zA-Z][a-zA-Z.0-9_]+@[a-zA-Z]{2,10}\.[a-zA-Z]{2,10}(\.[a-zA-Z]{2,10})?$/
+
+// console.log(pattern.test("dadasovsuleyman126@gmail.com"))
+
+// let a = 12;
+// let c = 16;
+
+// if(a>c){
+//   console.log(a);
+// }
+
+// O(1)
+
+
+// let b = 25; // 1
+
+// for(let i = 0; i < b;i++){ // n + 1
+//   console.log(i); // n 
+// }
+
+// n + n + 1 + 1 = 2n + 2 = n = O(n)
+
+// for(let i = 0;i<=20;i++){ // n 
+//   for(let j = 0; j<=20;j++){ // n^2
+//     console.log(i, j); // n^2
+//   }
+// }
+// 2n^2 + n = O(n^2)
+
+
+
+// for(let i = 0; i<20;i++){} // n
+
+// for(let j = 0; j<20;j++){} // n
+
+
+// O(n)
+
+// for(let i = 0; i<=100000; i++){ 
+//   for(let j = 0; j<=100000; j++){ // n^2
+  
+//   }
+// }
+
