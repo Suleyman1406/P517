262827790f21c8d1ea931d48617b9e2a0740926f
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-23 18:04:29 +0400

    js strings added

---
+++ javascript/strings/index.js
@@ -0,0 +1,40 @@
+// let str = "metin";
+
+// console.log(str.length);
+
+// let reversedStr = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversedStr += str[i];
+// }
+
+// console.log(reversedStr);
+
+// let str = "Hello my name is Code Academy.";
+// console.log(str.indexOf("e"));
+// console.log(str.lastIndexOf("e"));
+// console.log(str.indexOf("e", 5));
+
+// let str = "Hello my name is Code Academy.";
+
+// console.log(str.includes("nam2e"));
+
+// let str = "Hello my name is Code Academy.";
+
+// console.log(str.replace("e", "a"));
+// console.log(str.replaceAll("e", "a"));
+// console.log(str);
+
+// let str = " Hello my name is Code Academy.    ";
+
+// console.log(str.trim());
+
+// let str = "Hello-my-name is Code Academy.";
+
+// console.log(str.split(""));
+
+// let str = "Hello my name";
+
+// console.log(str.slice(-2, -4));
+// console.log(str.substring(1, 3));
+// console.log(str.substr(1, 2));
