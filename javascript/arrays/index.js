a1ba50b1c6810d70cf61ba2b619a42cafebcd283
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-24 00:19:03 +0400

    feat: arrays added

---
+++ javascript/arrays/index.js
@@ -0,0 +1,78 @@
+// let numbers = [3, 9, 12, 66, 12, 144];
+
+// numbers.length = 2;
+// console.log(numbers);
+
+// console.log(numbers[numbers.length - 1]);
+
+// console.log(numbers.at(-1));
+
+// let arr = ["asd"];
+
+// arr.push(18, 33, 99, 12);
+// arr.push("hello");
+
+// arr.unshift("test", "aasd", "evvel");
+
+// arr.push(19);
+// arr.push(120);
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.shift();
+// arr.shift();
+// arr.shift();
+// console.log(arr);
+
+// let nums = ["hello"];
+
+// (function (t) {
+//   t[0] = 12;
+// })(nums);
+
+// console.log(nums[0]);
+
+// let a = [12];
+// let c = "asd";
+
+// function func(b, d) {
+//   b[0] = 16;
+//   d[0] = "1";
+// }
+
+// func(a, c);
+
+// console.log(a[0]);
+// console.log(c);
+
+//  [ 1, 3, 7, 9, 8 ]
+
+// let myArr = new Array(10).fill(18);
+
+// console.log(myArr);
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+// console.log(arr[0][0][0]);
+// console.log(String(arr));
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [1, 2, 3];
+
+// console.log(arr1 === arr2);
+
+// let numbers = [12, 3, 66, 32, 128];
+
+// console.log(numbers.splice(0, 10));
+// console.log(numbers.splice(0, 1, 22, 88, 36, 99));
+// console.log(numbers.splice(0, 0, 66, 12, 188));
+// console.log(numbers);
+
+// let nums2 = [66, 123, 99];
+// console.log(numbers.concat(nums2));
+
+// console.log([1, 2, 3, 4].join(" ").split(" ").join("0"));
