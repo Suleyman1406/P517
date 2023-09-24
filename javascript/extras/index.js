2c3a57aebd6c4b0944f8b46074ebb7bc498296ff
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-09-24 20:05:37 +0400

    js extras added

---
+++ javascript/extras/index.js
@@ -0,0 +1,32 @@
+// let a = "asd";
+
+// console.log(!!!!!!!!!!!!!!!!!!a);
+
+// try {
+//   console.log(undefined.test);
+// } catch (err) {
+//   console.log(err.message);
+// } finally {
+// }
+
+// console.log("asdsd");
+
+// console.log(test);
+
+// var a =12;
+
+// var a = 15;
+
+// console.log(a);
+
+// if (test) {
+// }
+
+
+
+
+// function Hello(){
+//   var a = 12;
+// }
+
+// console.log(a);
