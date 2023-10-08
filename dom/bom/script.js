841f9402a02affb99e3150f44a74fa12de752a29
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-08 19:58:39 +0400

    dom bom added

---
+++ dom/bom/script.js
@@ -0,0 +1,27 @@
+// alert("isaaaa")
+// confirm("asd");
+// let message = prompt("Adinizi giriniz zeymet olmasa!");
+// console.log(message);
+
+// console.log(window.location);
+// window.location.reload();
+// console.log(location);
+
+// console.log(window.innerWidth);
+// console.log(window.outerWidth);
+
+// reklamCixart()
+
+// setTimeout(() => {
+//   reklamBagla();
+// }, 1000);
+
+// console.log("hello world");
+
+setTimeout(() => {
+  console.log("Bitti");
+}, 100);
+
+for (let i = 0; i < 100000; i++) {
+  console.log(i);
+}
