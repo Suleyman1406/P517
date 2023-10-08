b7b6b9e67c6f1325de92d4e1a43433f1f64ed03c
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-08 16:32:34 +0400

    dom events added

---
+++ dom/events/script.js
@@ -0,0 +1,47 @@
+const button = document.querySelector("button");
+
+// button.onclick = () => {
+//   console.log("Hello");
+// };
+// button.onclick = () => {
+//   console.log("Hello2");
+// };
+
+// const onBtnClick = () => {
+//   console.log("clicked");
+// };
+
+// function onBtnClick() {
+//   console.log("clicked");
+// }
+// function onBtnSecondClick() {
+//   console.log("second clicked");
+// }
+
+// button.addEventListener("click", onBtnClick);
+
+// button.addEventListener("click", onBtnSecondClick);
+
+// button.removeEventListener("click", onBtnClick);
+
+// const emailInputElement = document.querySelector("input");
+// let isControlPressed = false;
+
+// emailInputElement.addEventListener("keydown", (event) => {
+//   if (event.code === "ControlLeft") {
+//     isControlPressed = true;
+//   }
+// });
+// emailInputElement.addEventListener("keyup", (event) => {
+//   if (event.code === "ControlLeft") {
+//     isControlPressed = false;
+//   }
+// });
+
+// emailInputElement.addEventListener("keydown", (event) => {
+//   console.log(event.code);
+//   if (event.code === "KeyV" && isControlPressed) {
+//     // navigator.clipboard.writeText("kopyalamaq olmaz!");
+//     // navigator.clipboard.read
+//   }
+// });
