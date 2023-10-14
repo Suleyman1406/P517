636006f2ddf45ca0122bfd17056282266c8a6cbe
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-10-14 18:42:30 +0400

    js date time added

---
+++ dom/date-time/script.js
@@ -0,0 +1,12 @@
+// new Date(year, month, date, hours, minutes, seconds, ms)
+// const date = new Date(2012, 11, 12, 5, 36);
+// console.log(new Date());
+
+// const currentDate = new Date();
+// console.log("year", currentDate.getFullYear());
+// console.log("month", currentDate.getMonth());
+// console.log("date", currentDate.getDate());
+// console.log("hour", currentDate.getHours());
+// console.log("day", currentDate.getDay());
+// console.log("day", new Date().getTime());
+// console.log("day", Date.now());
